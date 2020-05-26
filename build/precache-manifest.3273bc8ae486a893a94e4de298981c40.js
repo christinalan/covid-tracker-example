8f3c114baeec19d9599df41a994e4924eb21b1b4
Your Name <christina.lan13@gmail.com>
2020-05-26 16:14:34 -0600

    idk

---
+++ build/precache-manifest.3273bc8ae486a893a94e4de298981c40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c599d8c0116c9a0185df29f5db8e9f7f",
+    "url": "/covid-tracker-example/index.html"
+  },
+  {
+    "revision": "5ffcd5680ab45a4d2c65",
+    "url": "/covid-tracker-example/static/css/main.c490c253.chunk.css"
+  },
+  {
+    "revision": "1d2142764cb07e3168d9",
+    "url": "/covid-tracker-example/static/js/2.a18ec5a6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker-example/static/js/2.a18ec5a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ffcd5680ab45a4d2c65",
+    "url": "/covid-tracker-example/static/js/main.e682ed44.chunk.js"
+  },
+  {
+    "revision": "273f0b26f972d03fa581",
+    "url": "/covid-tracker-example/static/js/runtime-main.8072fa67.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker-example/static/media/image.d7265326.png"
+  }
+]);
